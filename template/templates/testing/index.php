4431e5864a6e48dfbd9ee69835b17afe18b258e2
Jonathan Bennett <jbennett@ccistudios.com>
2011-04-01 02:11:32 -0400

    Best Template EVER!

--- template/templates/testing/index.php
+++ template/templates/testing/index.php
@@ -1,1 +1,14 @@
-hello world
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8" />
+
+	<jdoc:include type="head" />
+</head>
+
+<body>
+	<div style="width: 960px; margin: 0 auto;">
+		<jdoc:include type="component" />
+	</div>
+</body>
+</html>
